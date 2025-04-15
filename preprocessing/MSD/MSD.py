439907344a94ac3577222593def544456380ef30
ZahFay <alzahra_fayie@brown.edu>
2025-04-15 14:30:50 -0400

    unique artists

--- preprocessing/MSD/MSD.py
+++ preprocessing/MSD/MSD.py
@@ -5,12 +5,19 @@ import csv
 import sqlite3
 import pandas as pd
 
-def retrieve_artist(path):
+def retrieve_artist_trackID(path):
     con = sqlite3.connect(path)
     cur = con.cursor()
     df = pd.read_sql_query('SELECT artist_name,track_id FROM songs', con)
     df.to_csv('MSD_artists_trackIDS.csv', index=False)
+
+def retrieve_unique_artists(path):
+    con = sqlite3.connect(path)
+    cur = con.cursor()
+    df = pd.read_sql_query('SELECT artist_name FROM songs', con)
+    df.drop_duplicates(inplace= True)
+    df.to_csv('MSD_artists_unique.csv', index=False)
             
 if __name__ == "__main__":
     path = './track_metadata.db'
-    retrieve_artist(path)
+    retrieve_unique_artists(path)
